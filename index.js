c26e497202bd4e4e67d810090eda207d0a17fda5
daliakhalifa02 <daliakhalifa02@gmail.com>
2023-04-03 00:01:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
 
-// Write your code here
+// random
+let random = Math.floor(Math.random() * 10) + 1;
+
+// mod
+let num3 = 32;
+let num4 = 4;
+let mod = (num4%num3); 
+
+// max
+let max = Math.max(3,1,6,20,5,2);
